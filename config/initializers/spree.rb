3da4a4107a2b9ff3816f77d3bbf9ad760e7bb2c3
Konst0955 <3711733@gmail.com>
2018-05-04 12:04:36 +0300

    Initial commit2

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -9,3 +9,17 @@
 #
 # In order to initialize a setting do:
 # config.setting_name = 'new value'
+Spree.config do |config|
+  # Example:
+  # Uncomment to stop tracking inventory levels in the application
+  # config.track_inventory_levels = false
+ # config.logo = "logo.png"
+ 
+  config.currency = 'RUB'
+  country = Spree::Country.find_by_name('Russian Federation')
+  config.default_country_id = country.id if country.present?
+
+
+end
+
+Spree.user_class = "Spree::LegacyUser"
